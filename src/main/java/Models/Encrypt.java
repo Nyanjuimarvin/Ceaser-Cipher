ff6883494a95a90f301237b0cd75e0859ebc57a1
Nyanjuimarvin <marnyanjui@gmail.com>
2022-02-19 11:53:45 +0300

    Add encryption method and finsh RGR for encryption process

--- src/main/java/Models/Encrypt.java
+++ src/main/java/Models/Encrypt.java
@@ -1,10 +1,15 @@
 package Models;
 
+import javax.swing.*;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 public class Encrypt {
 
     private String wordToEncrypt;
+    private int shiftFactor;
+    private final char [] encryptCharArray = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
 
     public void setWordToEncrypt(String wordToEncrypt) {
         if(validEncryptString(wordToEncrypt)) {
@@ -17,8 +22,52 @@ public class Encrypt {
         return wordToEncrypt;
     }
 
+    public void setShiftFactor(int shiftFactor) {
+        this.shiftFactor = shiftFactor;
+    }
+
+    public int getShiftFactor() {
+        return shiftFactor;
+    }
+
     public boolean validEncryptString(String word){
         return word.matches("([a-zA-z]+|[a-zA-Z]+\\s[a-zA-Z]+)*");
+    }
+//Alphabet as List
+    public List<Character> alphabetLetters(){
+        List<Character> charsToShift = new ArrayList<>();
+        for(char letter: encryptCharArray){
+            charsToShift.add(letter);
+        }
+        return charsToShift;
+    }
+
+    public List<Character> collectCharacters(String word){
+        String capWord = word.toUpperCase();
+        List<Character> enteredWords = new ArrayList<>();
+        for( char capChar:capWord.toCharArray()){
+            enteredWords.add(capChar);
+        }
+     return enteredWords;
+    }
+   //Encryption Method
+    public char encryptCharacters(char initialLetter,int shift){
+        return alphabetLetters().get( ( alphabetLetters().indexOf(initialLetter) + shift ) % 26 );
+    };
+
+    //Encrypted Character List
+    public List<Character> encryptedWordList(List<Character>wordList,int shift){
+
+        List<Character> encryptedChars = new ArrayList<>();
+        for(char newChar:wordList) {
+            if(newChar == ' ') encryptedChars.add(' ');
+            for(char alphabetLetter:alphabetLetters()){
+                if( alphabetLetter == newChar){
+                    encryptedChars.add(encryptCharacters(alphabetLetter,shift));
+                }
+            }
+        }
+        return encryptedChars;
     }
 
 }
--- src/test/java/Models/EncryptTest.java
+++ src/test/java/Models/EncryptTest.java
@@ -4,6 +4,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class EncryptTest {
@@ -25,5 +29,34 @@ class EncryptTest {
     public void setWordToEncrypt_doesTheStringMatch() {
         myTestEncrypt.setWordToEncrypt("Hey there how are you doing");
         assertTrue(myTestEncrypt.validEncryptString(myTestEncrypt.getWordToEncrypt()));
+    }
+
+    @Test
+    @DisplayName("Collect Characters")
+    public void collectCharacters_areCharactersCollectedInALIst_ArrayList() {
+        List<Character> testCharacters = new ArrayList<>();
+        myTestEncrypt.setWordToEncrypt("Whats Happening People");
+        for (int i = 0; i < myTestEncrypt.getWordToEncrypt().length(); i++) {
+            testCharacters.add(myTestEncrypt.getWordToEncrypt().toUpperCase().charAt(i));
+        }
+        assertEquals(testCharacters,myTestEncrypt.collectCharacters(myTestEncrypt.getWordToEncrypt()));
+
+    }
+
+    @Test
+    @DisplayName("Encrypted List")
+    public void EnctyptedList_areEncryptedCharactersCollectedInALIst_ArrayList() {
+        List<Character> encryptedList = new ArrayList<>();
+        myTestEncrypt.setWordToEncrypt("What are you");
+
+        for(char collectedChar:myTestEncrypt.collectCharacters(myTestEncrypt.getWordToEncrypt())){
+            if( collectedChar == ' ' )  encryptedList.add(' ');
+            for (char alphabetChar:myTestEncrypt.alphabetLetters() ) {
+                if( alphabetChar == collectedChar ){
+                    encryptedList.add(myTestEncrypt.encryptCharacters(collectedChar,3));
+                }
+            }
+        }
+        assertEquals(encryptedList,myTestEncrypt.encryptedWordList(myTestEncrypt.collectCharacters(myTestEncrypt.getWordToEncrypt()),3));
     }
 }
