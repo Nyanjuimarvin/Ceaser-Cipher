bcd79eee53a5723500a7929c3262b33f1fee5644
Nyanjuimarvin <marnyanjui@gmail.com>
2022-02-19 19:36:59 +0300

    Create new instances of Encrypt and Decrypt in runner class

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,9 @@
+import Models.Decrypt;
+import Models.Encrypt;
+
 public class App {
+    public static void main(String[] args) throws Exception {
+        Encrypt encrypt = new Encrypt();
+        Decrypt decrypt = new Decrypt();
+    }
 }
